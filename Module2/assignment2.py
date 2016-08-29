9eff8e2db90644daa59c6f23893a9c52c6f168ed
torenunez <tore.nunez@gmail.com>
2016-08-28 22:21:31 -0500

    complete

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 # TODO: Load up the 'tutorial.csv' dataset
 #
-# .. your code here ..
+df = pd.read_csv("/Users/torenunez/PycharmProjects/DAT210x//Module2/Datasets/tutorial.csv")
 
 
 
 # TODO: Print the results of the .describe() method
 #
-# .. your code here ..
+print df.describe()
 
 
 
@@ -16,5 +16,5 @@ import pandas as pd
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
 #
-# .. your code here ..
+print df.loc[2:4,'col3']
 
